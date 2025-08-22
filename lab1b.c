68261c0049f19987992b9386dd802317c721f269
Techie-calster <gauravkumar23200200@gmail.com>
2025-08-22 17:03:03 +0530

    Updated

--- lab1b.c
+++ lab1b.c
@@ -49,6 +49,13 @@ int main(){
     for(int i=0;i<iterations;i++){
     int x= binarySearch(arr, 0 ,n-1,key);
     }
+    end=clock();
+
+    time_taken=((double)(end-start)/CLOCKS_PER_SEC);
+    double avg_ms= (double)(time_taken/iterations)*1000.0;  
+    printf("Time taken for n=%d is %lf ms (average per search)\n", key, avg_ms);
+
+    free(arr);
     end= clock();
     
     time_taken=(double)(end-start)/CLOCKS_PER_SEC;
@@ -57,5 +64,4 @@ int main(){
     free(arr);
     return 0;
 }
-//10,100,1000,5000,10000,50000
 
--- lab1b.py
+++ lab1b.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 x_values=[10,100,1000,5000,10000,50000]
-time_values=[0.009000,0.030000,0.047000,0.090000,0.088000,0.092000] # in seconds
+time_values=[0.000014,0.000029,0.000045,0.000066,0.000075,0.000083] # in seconds
 plt.plot(x_values, time_values, marker='o')
 plt.title("Linear Search: Time vs Number of Elements")
 plt.xlabel("Number of Elements (n)")
